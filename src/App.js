691df598b40cc93ba0f5e8e583a62015d3a0fe5f
NISHANTH HARSHA 69 <nishanthharsha23@gmail.com>
2024-09-10 13:51:48 +0530

    fix2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './Components/Homepage';
 import Contact from './Components/Contact';
@@ -8,14 +8,14 @@ import Resume from './Components/Resume';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/works" element={<Works />} />
         <Route path="/resume" element={<Resume />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
